40e2b51a598c83baf5b7d6b15c13b840aff86fb2
Shimil Shaji Abraham <62107737+ShimilSAbraham@users.noreply.github.com>
2021-01-12 15:33:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+function UserLogin(){
+    var getValue = document.getElementById('name_user').value;
+   console.log(getValue);
+   localStorage.setItem("key1",getValue);
+}
+
+function HospitalLogin(){
+    var getValue = document.getElementById('name_hos').value;
+   console.log(getValue);
+   localStorage.setItem("key2",getValue);
+}
